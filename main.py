38726c1d86e1210141c453219051df2589bf8ac4
varunchopra11 <varunchopra2003@gmail.com>
2025-10-29 03:06:55 +0530

    Refactor CORS middleware to use a defined origins list

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
+origins = [
+    "https://app.supogen.com",
+    "http://localhost:8000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
